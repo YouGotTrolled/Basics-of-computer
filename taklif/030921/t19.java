a2972ea9f286fe634ba7e6740194353cf876cf1b
Abdoll <96470320+YouGotTrolled@users.noreply.github.com>
2024-12-06 06:29:22 +0330

    Update t19.java

--- taklif/030921/t19.java
+++ taklif/030921/t19.java
@@ -1,9 +1,17 @@
 import java.util.Scanner;
-public class t9 {
+public class t19 {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         System.out.println("عدد را وارد کنید");
         int x=scan.nextInt();
-        System.out.println(((x*1.8)+32));
+        if(x>0){
+            System.out.println("عدد مثبت است");
+        }
+        else if(x<0){
+            System.out.println("عدد منفی است");
+        }
+        else{
+            System.out.println("صفر");
+        }
     }
 }
