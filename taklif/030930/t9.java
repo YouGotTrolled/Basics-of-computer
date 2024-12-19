34acf237905315b8b7407bf0b4aa2b9981a71a82
Abdoll <mohammadamin.dbz@gmail.com>
2024-12-20 02:55:27 +0330

    Create t9.java

---
+++ taklif/030930/t9.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class t9{
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("enter the number:");
+        int n=scan.nextInt();
+        int sum;
+        int i;
+        for(i=2;i<n;i++){
+            sum=1;
+            for(int j=2;j<i;j++){
+                if(i%j==0){
+                    sum+=j;
+                }
+            }
+            if(i==6){
+                System.out.print("(6");
+            }
+            else if(i==sum){
+                System.out.print(","+i);
+            }
+        }
+        if(i>6)
+            System.out.print(")");
+    }
+}
