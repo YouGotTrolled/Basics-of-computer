1357fd623dc14a217320df854fb9a12554135053
Abdoll <96470320+YouGotTrolled@users.noreply.github.com>
2025-01-21 01:36:55 +0330

    up

---
+++ final/un3.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+public class un3{
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("String?");
+        String in=scan.nextLine();
+        first(in);
+    }
+    public static void first(String a){
+        String temp;
+        StringTokenizer spt = new StringTokenizer(a);
+        int i=0;
+        while(spt.hasMoreTokens()){
+            i++;
+            spt.nextToken();
+        }
+        spt = new StringTokenizer(a);
+        String[] words=new String[i];
+        int[] repeat=new int[i];
+        i=0;
+        while(spt.hasMoreTokens()){
+            boolean flag=true;
+            temp=spt.nextToken();
+            int j=0;
+            for(;j<i&&flag;j++){
+                if(words[j].equals(temp)){
+                    flag=false;
+                }
+            }
+            if(!flag){
+                repeat[--j]++;
+            }else{
+                words[i]=temp;
+                repeat[i++]=1;
+            }
+        }
+        for(i=0;i<words.length;i++){
+            System.out.println(words[i]+"="+repeat[i]);
+        }
+    }
+}
